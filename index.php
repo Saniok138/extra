de89f1503e85e3a6030250f38a9424829bbbfc00
Saniok138 <112718886+Saniok138@users.noreply.github.com>
2024-03-05 18:01:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Показ данных из базы данных</title>
+</head>
+<body>
+
 <?php
 $host = "sql8.freesqldatabase.com";
 $user = "sql8687824";
@@ -9,6 +18,7 @@ $conn = mysqli_connect($host, $user, $password, $database);
 if (!$conn) {
     die("Ошибка подключения: " . mysqli_connect_error());
 }
+
 $query = "SELECT * FROM pytania";
 $result = mysqli_query($conn, $query);
 
@@ -16,11 +26,25 @@ if (!$result) {
     die("Ошибка запроса: " . mysqli_error($conn));
 }
 
-// Обработка результатов запроса
+// Генерация HTML на основе результатов запроса
+echo "<table border='1'>
+        <tr>
+            <th>Numer</th>
+            <th>Pytanie</th>
+        </tr>";
+
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "Pytanie: " . $row["idp"] . " " . $row["pytanie"] . "<br>";
+    echo "<tr>
+            <td>" . $row["idp"] . "</td>
+            <td>" . $row["pytanie"] . "</td>
+          </tr>";
 }
+
+echo "</table>";
 
 // Закрытие соединения
 mysqli_close($conn);
 ?>
+
+</body>
+</html>
