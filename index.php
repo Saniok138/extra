2dfaabb3dad6b453f0836caf6b0f268db16f6c0f
Saniok138 <112718886+Saniok138@users.noreply.github.com>
2024-03-06 14:40:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,42 +8,12 @@
 <body>
 
 <?php
-$host = "sql8.freesqldatabase.com";
-$user = "sql8687824";
-$password = "HKiuRJzgH7";
-$database = "sql8687824";
 
-$conn = mysqli_connect($host, $user, $password, $database);
-
-if (!$conn) {
-    die("Ошибка подключения: " . mysqli_connect_error());
-}
-
-$query = "SELECT * FROM pytania";
-$result = mysqli_query($conn, $query);
-
-if (!$result) {
-    die("Ошибка запроса: " . mysqli_error($conn));
-}
-
-// Генерация HTML на основе результатов запроса
 echo "<table border='1'>
         <tr>
             <th>Id</th>
             <th>Pytania</th>
         </tr>";
-
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>
-            <td>" . $row["idp"] . "</td>
-            <td>" . $row["pytanie"] . "</td>
-          </tr>";
-}
-
-echo "</table>";
-
-// Закрытие соединения
-mysqli_close($conn);
 ?>
 
 </body>
